eb251c586ce14eeceb832b6d73e4129a840295ed
Desdevises Simon <simon.desdevises@gmail.com>
2023-07-02 18:31:16 +0200

    delete menu hover

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import Menu from "./Menu";
 import Title from "./Title";
 import EducationDiscount from "./EducationDiscount";
 
 export default function Header(): JSX.Element {
-  const [menuHover, setMenuHover] = useState<number>(-1);
-
   return (
     <div>
-      <Menu setMenuHover={setMenuHover} />
+      <Menu />
       <Title />
       <EducationDiscount />
     </div>
